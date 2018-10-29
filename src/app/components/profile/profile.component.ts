501750cc652d66e71ae75588eafca64e57ee70b9
vinnyotach7 <vinnyotach7@gmail.com>
2018-10-29 09:14:42 +0300

    modification to my component/src

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../services/profile.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+profile: any[string];
+repos: any[];
+
+  constructor(private profileService: ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      // this.repos = repos;
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
